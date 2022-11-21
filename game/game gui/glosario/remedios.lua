1806f54002d37dc3ce96405a4da7659963e3ede8
Pablo Sonaglioni <pablosonaglioni@gmail.com>
2022-11-21 15:07:21 -0300

    Los botones cambian de color.-

--- game/game gui/glosario/remedios.lua
+++ game/game gui/glosario/remedios.lua
@@ -1,9 +1,6 @@
 local red = {"red", vmath.vector4(255,0,0,1)};
 local green = {"green", vmath.vector4(0,128,0,1)};
 local blue = {"blue", vmath.vector4(0,0,255,1)};
-local red_c = vmath.vector4(255,0,0,1);
-local green_c = vmath.vector4(0,128,0,1);
-local blue_c = vmath.vector4(0,0,255,1);
 
 -- vacc pasti curita comida
 local M = {};
@@ -16,7 +13,8 @@ M.estados[4] = hash("congelado");
 M.estados[5] = hash("debilitado");
 	-- ENVENENADO --
 M[1] = {}
-M[1].orden = {"vacc", "pasti", "curita", "comida"}
+M[1].img = {"vacc", "pasti", "curita", "comida"}
+M[1].orden = {hash("vacuna"), hash("pasti"), hash("curita"), hash("comida")};
 M[1][1] = red
 M[1][2] = green
 M[1][3] = blue
@@ -24,7 +22,7 @@ M[1][4] = red
 
 -- PARALIZADO --
 M[2] = {}
-M[2].orden = {"curita", "vacc", "pasti", "comida"}
+M[2].img = {"curita", "vacc", "pasti", "comida"}
 M[2][1] = green
 M[2][2] = blue
 M[2][3] = red
