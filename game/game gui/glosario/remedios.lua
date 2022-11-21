b2001dd07196580afc0c73a957297f5c7957ccac
Pablo Sonaglioni <pablosonaglioni@gmail.com>
2022-11-21 18:13:20 -0300

    Va queriendo.-

--- game/game gui/glosario/remedios.lua
+++ game/game gui/glosario/remedios.lua
@@ -4,7 +4,7 @@ local blue = {"blue", vmath.vector4(0,0,255,1)};
 
 -- vacc pasti curita comida
 local M = {};
-
+M.bicho = nil;
 M.estados = {};
 M.estados[1] = hash("envenenado") ;
 M.estados[2] = hash("paralizado");
