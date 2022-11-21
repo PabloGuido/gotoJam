7b2861cbd692c7f68e1bd075f06434b891222f85
Pablo Sonaglioni <pablosonaglioni@gmail.com>
2022-11-21 14:07:29 -0300

    Avanzando.-

--- game/game gui/glosario/remedios.lua
+++ game/game gui/glosario/remedios.lua
@@ -1,6 +1,6 @@
-local red = "red";
-local green = "green";
-local blue = "blue";
+local red = {"red", vmath.vector4(255,0,0,1)};
+local green = {"green", vmath.vector4(0,128,0,1)};
+local blue = {"blue", vmath.vector4(0,0,255,1)};
 local red_c = vmath.vector4(255,0,0,1);
 local green_c = vmath.vector4(0,128,0,1);
 local blue_c = vmath.vector4(0,0,255,1);
@@ -17,11 +17,18 @@ M.estados[5] = hash("debilitado");
 	-- ENVENENADO --
 M[1] = {}
 M[1].orden = {"vacc", "pasti", "curita", "comida"}
-M[1][1] = {red, red_c};
-M[1][2] = {green, green_c};
-M[1][3] = {blue, blue_c};
-M[1][4] = {red, red_c};
+M[1][1] = red
+M[1][2] = green
+M[1][3] = blue
+M[1][4] = red
 
+-- PARALIZADO --
+M[2] = {}
+M[2].orden = {"curita", "vacc", "pasti", "comida"}
+M[2][1] = green
+M[2][2] = blue
+M[2][3] = red
+M[2][4] = green
 
 
 
