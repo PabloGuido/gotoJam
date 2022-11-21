ca3196124d6c9799e9e94b657067a8ce55aa0566
Pablo Sonaglioni <pablosonaglioni@gmail.com>
2022-11-21 13:38:55 -0300

    Armando los remedios.-

--- game/game gui/glosario/remedios.lua
+++ game/game gui/glosario/remedios.lua
@@ -2,18 +2,25 @@ local red = "red";
 local green = "green";
 local blue = "blue";
 local red_c = vmath.vector4(255,0,0,1);
-local green_c = vmath.vector4(128,0,0,1);
+local green_c = vmath.vector4(0,128,0,1);
 local blue_c = vmath.vector4(0,0,255,1);
 
 -- vacc pasti curita comida
 local M = {};
 
+M.estados = {};
+M.estados[1] = "envenenado";
+M.estados[2] = "paralizado";
+M.estados[3] = "quemado";
+M.estados[4] = "congelado";
+M.estados[5] = "debilitado";
+
 M.envenenado = {}
 M.envenenado.orden = {"vacc", "pasti", "curita", "comida"}
-M.envenenado.vacc = {red, red_c};
-M.envenenado.pasti = {green, green_c};
-M.envenenado.curita = {blue, blue_c};
-M.envenenado.comida = {red, red_c};
+M.envenenado[1] = {red, red_c};
+M.envenenado[2] = {green, green_c};
+M.envenenado[3] = {blue, blue_c};
+M.envenenado[4] = {red, red_c};
 
 
 
