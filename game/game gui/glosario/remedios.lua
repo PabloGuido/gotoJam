45399e1990e36379fb4eb5390c9e72fb26d403f1
Pablo Sonaglioni <pablosonaglioni@gmail.com>
2022-11-21 12:47:15 -0300

    Algunos placeholder.

---
+++ game/game gui/glosario/remedios.lua
@@ -0,0 +1,19 @@
+local red = "red";
+local green = "green";
+local blue = "blue";
+-- vacc pasti curita comida
+local M = {};
+
+M.envenenado = {}
+M.envenenado.orden = {"vacc", "pasti", "curita", "comida"}
+M.envenenado.vacc = red;
+M.envenenado.pasti = green;
+M.envenenado.curita = blue;
+M.envenenado.comida = red;
+
+
+
+
+
+
+return M;
