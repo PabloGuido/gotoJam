b7c3588a6d7a11a226fafaddef4baecd39b9acf7
Pablo Sonaglioni <pablosonaglioni@gmail.com>
2022-11-21 19:11:34 -0300

    Barra de vida va ok.

---
+++ game/game gui/barra de vida/barra_de_vida.go
@@ -0,0 +1,15 @@
+components {
+  id: "barra_de_vida"
+  component: "/game/game gui/barra de vida/barra_de_vida.gui"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
