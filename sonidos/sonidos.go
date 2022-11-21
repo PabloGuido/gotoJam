113cacb0ec3e8505fe6d38f57bb7ab30951e6694
Pablo Sonaglioni <pablosonaglioni@gmail.com>
2022-11-21 18:32:21 -0300

    Algo de sonido para check y alguna cosa.-

---
+++ sonidos/sonidos.go
@@ -0,0 +1,61 @@
+components {
+  id: "sonidos"
+  component: "/sonidos/sonidos.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "ok"
+  type: "sound"
+  data: "sound: \"/sonidos/ok.ogg\"\n"
+  "looping: 0\n"
+  "group: \"master\"\n"
+  "gain: 1.0\n"
+  "pan: 0.0\n"
+  "speed: 1.0\n"
+  "loopcount: 0\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "not_ok"
+  type: "sound"
+  data: "sound: \"/sonidos/not_ok.ogg\"\n"
+  "looping: 0\n"
+  "group: \"master\"\n"
+  "gain: 1.0\n"
+  "pan: 0.0\n"
+  "speed: 1.0\n"
+  "loopcount: 0\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
