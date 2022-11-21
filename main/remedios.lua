047908897c4cd1ed1441540ccef8d9366e6c244a
Pablo Sonaglioni <pablosonaglioni@gmail.com>
2022-11-21 02:52:33 -0300

    Armando el glosario.-

---
+++ main/remedios.lua
@@ -0,0 +1,13 @@
+local M = {};
+-- vacc pasti curita comida
+M.envenenado = {}
+M.envenenado.orden = {"vacc", "pasti", "curita", "comida"}
+M.envenenado.color = {"red", "green", "blue", "red"}
+
+
+
+
+
+
+
+return M;
