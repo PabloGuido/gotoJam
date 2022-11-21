60464d697dd37f1f6a002eb496fcef5255b4f6d1
Pablo Sonaglioni <pablosonaglioni@gmail.com>
2022-11-21 19:49:25 -0300

    Va queriendo.-

--- sonidos/sonidos.go
+++ sonidos/sonidos.go
@@ -59,3 +59,26 @@ embedded_components {
     w: 1.0
   }
 }
+embedded_components {
+  id: "remedio"
+  type: "sound"
+  data: "sound: \"/sonidos/remedio.ogg\"\n"
+  "looping: 0\n"
+  "group: \"master\"\n"
+  "gain: 0.5\n"
+  "pan: 0.0\n"
+  "speed: 1.0\n"
+  "loopcount: 0\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
